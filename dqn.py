Fix for one-hot action selection bug, adding CartPole to available environments, and switching to exponential moving average weight transfer for target networks

dqn.py
@@ -16,15 +16,15 @@ FRAME_SKIP = 4
 REPLAY_MEMORY_CAPACITY = 50000  # fifty thousand
 
 # hyperparameters
-ALPHA = 25e-5             # initial learning rate
+ALPHA = 1e-4              # initial learning rate
 GAMMA = 0.9               # discount factor
 EPSILON = 1e-2            # numerical stability
+TAU = 0.001               # target network weight transfer decay
 RMS_DECAY = 0.95          # rmsprop decay
 MOMENTUM = 0.95           # rmsprop momentum
 FINAL_EXPLORATION = 0.1   # final exploration rate
-EXPLORATION_DECAY = 5e-6  # linear decay of exploration
+EXPLORATION_DECAY = 1e-6  # linear decay of exploration
 BATCH_SIZE = 64           # size of training batch
-TARGET_UPDATE = 1000      # iterations per target network update
 
 # scope names
 DQN_SCOPE = 'dqn'
@@ -36,6 +36,7 @@ EVALUATION_SCOPE = 'evaluator'
 CONV1 = 'conv1'
 CONV2 = 'conv2'
 CONV3 = 'conv3'
+FC = 'fully_connected'
 HIDDEN = 'hidden'
 OUTPUT = 'output'
 
@@ -47,16 +48,21 @@ GPU_MEMORY_FRACTION = 0.5
 class DQN():
 
     def __init__(self, env_type, state_dims, num_actions):
-        self.replay_memory = ReplayMemory(REPLAY_MEMORY_CAPACITY,
-                                          [state_dims[0], state_dims[1]*FRAME_STACK],
-                                          num_actions)
+        if env_type == EnvTypes.ATARI:
+            state_size = [state_dims[0], state_dims[1]*FRAME_STACK, state_dims[2]]
+        elif env_type == EnvTypes.STANDARD:
+            state_size = state_dims
+        self.replay_memory = ReplayMemory(REPLAY_MEMORY_CAPACITY, state_size)
         self.exploration = 1.0
         self.train_iter = 0
+        self.env_type = env_type
 
         if env_type == EnvTypes.ATARI:
             buffer_size = FRAME_STACK*FRAME_SKIP
-            self.observation_buffer = [np.zeros((state_dims[0], state_dims[1]))
+            self.observation_buffer = [np.zeros((state_dims[0], state_dims[1], state_dims[2]))
                                        for _ in range(buffer_size)]
+        else:
+            self.observation_buffer = [np.zeros((state_dims[0]))]
 
         self.config = tf.ConfigProto()
         self.config.gpu_options.per_process_gpu_memory_fraction = GPU_MEMORY_FRACTION
@@ -66,17 +72,12 @@ class DQN():
         self.dqn_vars = dict()
         with tf.variable_scope(DQN_SCOPE):
             if env_type == EnvTypes.ATARI:
-                self.x = tf.placeholder(tf.float32, shape=[None, state_dims[0],
-                                                           state_dims[1]*FRAME_STACK, 1])
-                self.conv1 = conv2d(self.x, 8, 4, 32, CONV1, var_dict=self.dqn_vars)
-                self.conv2 = conv2d(self.conv1, 4, 2, 64, CONV2, var_dict=self.dqn_vars)
-                self.conv3 = conv2d(self.conv2, 3, 1, 64, CONV3, var_dict=self.dqn_vars)
-                conv_shape = self.conv3.get_shape().as_list()
-                flatten = [-1, conv_shape[1]*conv_shape[2]*conv_shape[3]]
-                self.initial_layers = tf.reshape(self.conv3, flatten)
+                self.x, self.initial_layers = self.add_atari_layers(state_dims, self.dqn_vars)
+            elif env_type == EnvTypes.STANDARD:
+                self.x, self.initial_layers = self.add_standard_layers(state_dims, self.dqn_vars)
 
             # add final hidden layers
-            self.hid = fc(self.initial_layers, 512, HIDDEN, var_dict=self.dqn_vars)
+            self.hid = fc(self.initial_layers, 128, HIDDEN, var_dict=self.dqn_vars)
             self.q = fc(self.hid, num_actions, OUTPUT,
                         var_dict=self.dqn_vars, activation=False)
             
@@ -86,16 +87,13 @@ class DQN():
         self.target_vars = dict()
         with tf.variable_scope(TARGET_SCOPE):
             if env_type == EnvTypes.ATARI:
-                self.t_x = tf.placeholder(tf.float32, shape=[None, state_dims[0],
-                                                             state_dims[1]*FRAME_STACK, 1])
-                self.t_conv1 = conv2d(self.t_x, 8, 4, 32, CONV1, var_dict=self.target_vars)
-                self.t_conv2 = conv2d(self.t_conv1, 4, 2, 64, CONV2, var_dict=self.target_vars)
-                self.t_conv3 = conv2d(self.t_conv2, 3, 1, 64, CONV3, var_dict=self.target_vars)
-                conv_shape = self.t_conv3.get_shape().as_list()
-                flatten = [-1, conv_shape[1]*conv_shape[2]*conv_shape[3]]
-                self.t_initial_layers = tf.reshape(self.t_conv3, flatten)
+                self.t_x, self.t_initial_layers = self.add_atari_layers(state_dims,
+                                                                        self.target_vars)
+            elif env_type == EnvTypes.STANDARD:
+                self.t_x, self.t_initial_layers = self.add_standard_layers(state_dims,
+                                                                           self.target_vars)
 
-            self.t_hid = fc(self.t_initial_layers, 512, HIDDEN, var_dict=self.target_vars)
+            self.t_hid = fc(self.t_initial_layers, 128, HIDDEN, var_dict=self.target_vars)
             self.t_q = fc(self.t_hid, num_actions, OUTPUT,
                           var_dict=self.target_vars, activation=False)
 
@@ -106,13 +104,17 @@ class DQN():
         with tf.variable_scope(TRANSFER_SCOPE):
             for variable in self.dqn_vars.keys():
                 target_variable = TARGET_SCOPE + variable[len(DQN_SCOPE):]
-                target_assign = self.target_vars[target_variable].assign(self.dqn_vars[variable])
+                decay = tf.mul(1 - TAU, self.target_vars[target_variable])
+                update = tf.mul(TAU, self.dqn_vars[variable])
+                new_target_weight = tf.add(decay, update)
+                target_assign = self.target_vars[target_variable].assign(new_target_weight)
                 self.assign_ops.append(target_assign)
 
         # build dqn evaluation
         with tf.variable_scope(EVALUATION_SCOPE):
             # one-hot action selection
-            self.action = tf.placeholder(tf.float32, shape=[None, num_actions])
+            self.action = tf.placeholder(tf.int32, shape=[None])
+            self.action_one_hot = tf.one_hot(self.action, num_actions)
             # reward
             self.reward = tf.placeholder(tf.float32, shape=[None, 1])
             # terminal state
@@ -120,11 +122,12 @@ class DQN():
 
             self.target = tf.add(self.reward, tf.mul(GAMMA, tf.mul(self.nonterminal,
                           tf.reduce_max(self.t_q, 1, True))))
-            self.predict = tf.reduce_sum(tf.mul(self.action, self.q), 1, True)
+            self.predict = tf.reduce_sum(tf.mul(self.action_one_hot, self.q), 1, True)
             self.error = tf.reduce_mean(mse(self.predict, self.target))
 
             tf.scalar_summary('error', self.error)
         
+        val_print = tf.Print(self.error, [self.predict, self.target])
         self.optimize = tf.train.RMSPropOptimizer(ALPHA, decay=RMS_DECAY, momentum=MOMENTUM,
                         epsilon=EPSILON).minimize(self.error, var_list=self.dqn_vars.values())
 
@@ -136,10 +139,25 @@ class DQN():
 
         # initialize variables
         self.sess.run(tf.initialize_all_variables())
+
+    def add_atari_layers(self, dims, var_dict):
+        x = tf.placeholder(tf.float32, shape=[None, dims[0], dims[1]*FRAME_STACK, 1])
+        conv1 = conv2d(x, 8, 4, 32, CONV1, var_dict=var_dict)
+        conv2 = conv2d(conv1, 4, 2, 64, CONV2, var_dict=var_dict)
+        conv3 = conv2d(conv2, 3, 1, 64, CONV3, var_dict=var_dict)
+        conv_shape = conv3.get_shape().as_list()
+        flatten = [-1, conv_shape[1]*conv_shape[2]*conv_shape[3]]
+        return x, tf.reshape(conv3, flatten)
+
+    def add_standard_layers(self, dims, var_dict):
+        x = tf.placeholder(tf.float32, shape=[None, dims[0]])
+        fc1 = fc(x, 256, FC, var_dict=var_dict)
+        return x, fc1
         
     def process_observation(self, observation):
-        # convert to normalized luminance and downscale
-        observation = downscale(rgb_to_luminance(observation), 2)
+        if self.env_type == EnvTypes.ATARI:
+            # convert to normalized luminance and downscale
+            observation = downscale(rgb_to_luminance(observation), 2)
 
         # push the new observation onto the buffer
         self.observation_buffer.pop(len(self.observation_buffer)-1)
@@ -152,10 +170,12 @@ class DQN():
         return stacked_state
 
     def _predict(self):
-        stacked_state = self._get_stacked_state()
-        stacked_state = np.expand_dims(stacked_state, axis=0)
-        stacked_state = np.expand_dims(stacked_state, axis=3)
-        return np.argmax(self.sess.run(self.q, feed_dict={self.x: stacked_state}))
+        if self.env_type == EnvTypes.ATARI:
+            state = self._get_stacked_state()
+        else:
+            state = self.observation_buffer[0]
+        state = np.expand_dims(state, axis=0)
+        return np.argmax(self.sess.run(self.q, feed_dict={self.x: state}))
 
     def training_predict(self, env, observation):
         self.process_observation(observation)
@@ -174,7 +194,10 @@ class DQN():
         return self._predict()
 
     def notify_state_transition(self, action, reward, done):
-        state = self._get_stacked_state()
+        if self.env_type == EnvTypes.ATARI:
+            state = self._get_stacked_state()
+        else:
+            state = self.observation_buffer[0]
         self.replay_memory.add_state_transition(state, action, reward, done)
         if done:
             # flush the observation buffer
@@ -183,25 +206,22 @@ class DQN():
 
     def batch_train(self):
         # sample batch from replay memory
-        states, actions, rewards, terminals, newstates = self.replay_memory.sample(BATCH_SIZE)
-        states = np.expand_dims(states, axis=3)
-        newstates = np.expand_dims(newstates, axis=3)
-        rewards = np.expand_dims(rewards, axis=1)
-        terminals = np.expand_dims(terminals, axis=1)
-        nonterminals = 1 - terminals
+        state, action, reward, terminal, newstate = self.replay_memory.sample(BATCH_SIZE)
+        reward = np.expand_dims(reward, axis=1)
+        terminal = np.expand_dims(terminal, axis=1)
+        nonterminal = 1 - terminal
 
         # update target network weights
-        if self.train_iter % TARGET_UPDATE == 0:
-            self.sess.run(self.assign_ops)
+        self.sess.run(self.assign_ops)
 
         # run neural network training step
         if self.train_iter % SUMMARY_PERIOD == 0:
-            summary, _ = self.sess.run([self.summaries, self.optimize], feed_dict={self.x:states,
-                                       self.t_x:newstates, self.action:actions,
-                                       self.reward:rewards, self.nonterminal:nonterminals})
+            summary, _ = self.sess.run([self.summaries, self.optimize], feed_dict={self.x:state,
+                                       self.t_x:newstate, self.action:action,
+                                       self.reward:reward, self.nonterminal:nonterminal})
             self.writer.add_summary(summary, self.train_iter)
         else:
-            self.sess.run(self.optimize, feed_dict={self.x:states, self.t_x:newstates,
-                          self.action:actions, self.reward:rewards, self.nonterminal:nonterminals})
+            self.sess.run(self.optimize, feed_dict={self.x:state, self.t_x:newstate,
+                          self.action:action, self.reward:reward, self.nonterminal:nonterminal})
 
         self.train_iter += 1

utils.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 def rgb_to_luminance(image):
     luminance = 0.2126*image[:,:,0] + 0.7152*image[:,:,1] + 0.0722*image[:,:,2]
     luminance /= 255.0
-    return luminance
+    return np.expand_dims(luminance, axis=3)
 
 def downscale(image, factor):
-    return image[::factor, ::factor]
+    return image[::factor, ::factor, :]